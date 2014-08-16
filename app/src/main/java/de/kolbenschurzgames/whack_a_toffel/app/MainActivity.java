2f48ac422dda0b531671b1182992f739379f19d8
alex-friedl <Alexander Friedl>
2014-08-16 13:39:32 +0200

    Initial Android-Gradle app frame

---
+++ app/src/main/java/de/kolbenschurzgames/whack_a_toffel/app/MainActivity.java
@@ -0,0 +1,15 @@
+package de.kolbenschurzgames.whack_a_toffel.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
